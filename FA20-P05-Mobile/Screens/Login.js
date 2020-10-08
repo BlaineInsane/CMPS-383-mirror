b682c2c855e961067b8ea5cd51ca3b14f7df6ba6
Blaine <blaine.prestenbach@selu.edu>
2020-10-07 20:55:11 -0500

    Moved ApiLogin to it's own file

--- FA20-P05-Mobile/Screens/Login.js
+++ FA20-P05-Mobile/Screens/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { render } from "react-dom";
 import {
   StyleSheet,
   View,
@@ -7,8 +6,7 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import axios from 'axios';
-import { BASE_URL } from '../env.js';
+import ApiLogin from "../ApiCalls/ApiLogin";
 
 //TODO: manage logged in user(cookies?)
 //      create frontend stuff if login returns 400
@@ -26,36 +24,46 @@ export default class Login extends Component {
     this.setState({ Password: event });
   };
 
-  handleSubmit = event => {
-    const Username = this.state.Username;
-    const Password = this.state.Password;
-
-    // TODO: move axios call to ApiCalls/ApiLogin.js
-    // I(Blaine) can't get that to work
-    const loginUrl = `${BASE_URL}/api/authentication/login/`;
-
-    axios.post(loginUrl, { Username, Password }, {
-      headers: {
-        'content-type': 'application/json',
-      },
-    })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+  handleSubmit = () => {
+    const loginResult = ApiLogin(this.state.Username, this.state.Password);
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput placeholder="Username" style={styles.textBox} onChangeText={this.handleUsernameChange}></TextInput>
-        <TextInput placeholder="Password" style={styles.textBox} onChangeText={this.handlePasswordChange}></TextInput>
+        <TextInput
+          placeholder="Username"
+          style={styles.textBox}
+          onChangeText={this.handleUsernameChange}
+        ></TextInput>
+        <TextInput
+          placeholder="Password"
+          style={styles.textBox}
+          onChangeText={this.handlePasswordChange}
+        ></TextInput>
         <TouchableOpacity style={styles.button} onPress={this.handleSubmit}>
-          <Text style={[{ color: "white", textAlign: "center", textAlignVertical: 'center', fontSize: 18 }]}>Log in</Text>
+          <Text
+            style={[
+              {
+                color: "white",
+                textAlign: "center",
+                textAlignVertical: "center",
+                fontSize: 18,
+              },
+            ]}
+          >
+            Log in
+          </Text>
         </TouchableOpacity>
-        <TouchableOpacity><Text style={[{ marginTop: 20, textDecorationLine: 'underline', color: 'gray' }]}>Create an account</Text></TouchableOpacity>
+        <TouchableOpacity>
+          <Text
+            style={[
+              { marginTop: 20, textDecorationLine: "underline", color: "gray" },
+            ]}
+          >
+            Create an account
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -75,8 +83,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     marginBottom: 15,
     paddingHorizontal: 10,
-    borderBottomColor: 'rgba(125, 125, 125, 0.60)',
-    borderBottomWidth: 1
+    borderBottomColor: "rgba(125, 125, 125, 0.60)",
+    borderBottomWidth: 1,
   },
   button: {
     height: 35,
