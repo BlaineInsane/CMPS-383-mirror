0c0f9b8013dbd4ab3192da4854185f1e2cd7bc87
Joseph Fontenelle <joseph.fontenelle@selu.edu>
2020-10-07 23:59:40 -0500

    very basic spa nav and page descriptions

--- FA20.P05.Web/ClientApp/src/App.js
+++ FA20.P05.Web/ClientApp/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/publicData">View Public Data</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/publicData">
+            <PublicData />
+          </Route>
+          <Route path="/admin">
+            <Admins />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Hiiiiiiiiiiiii This is our homepage!</h2>;
+}
+
+function PublicData() {
+  return <h2>welcome internet view covid data here</h2>;
+}
+
+function Admins() {
+  return <h2>admins do stuff with users here</h2>;
+}
+
