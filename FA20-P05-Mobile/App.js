ed5a39283e19fe35513254a8c3f737f28843d6a6
Blaine <blaine.prestenbach@selu.edu>
2020-10-17 12:31:03 -0500

    Added loading spinner
    TODO: make it look nice

--- FA20-P05-Mobile/App.js
+++ FA20-P05-Mobile/App.js
@@ -3,10 +3,10 @@ import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { headerColor, buttonColor } from "./Screens/Main";
-import { UserContext } from "./UserContext";
+import { UserContext } from "./Context/UserContext";
+import { isLoadingContext } from "./Context/IsLoadingContext";
 import { useEffect } from "react";
 import { StatusBar } from "react-native";
-
 import statusBar from "./Screens/Main";
 import Main from "./Screens/Main";
 import Login from "./Screens/Login";
@@ -15,10 +15,12 @@ import PersonalData from "./Screens/PersonalData";
 import Staff from "./Screens/Staff";
 import RecordTemps from "./Screens/RecordTemps";
 import ApiMe from "./ApiCalls/ApiMe";
+import Spinner from "react-native-loading-spinner-overlay";
 
 const Stack = createStackNavigator();
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -30,60 +32,64 @@ function App() {
       let res = await ApiMe();
       if (res.status == "200") {
         setUser(res.data);
+        setIsLoading(false);
       }
     } catch {
-      //console.log(err);
+      setIsLoading(false);
     }
   };
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Main"
-            component={Main}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Login"
-            component={Login}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="PublicData"
-            component={PublicData}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="PersonalData"
-            component={PersonalData}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="Staff"
-            component={Staff}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen
-            name="RecordTemps"
-            component={RecordTemps}
-            options={{
-              headerShown: false,
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <isLoadingContext.Provider value={{ isLoading, setIsLoading }}>
+        <Spinner visible={isLoading} textContent={"Loading"} />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Main"
+              component={Main}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="PublicData"
+              component={PublicData}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="PersonalData"
+              component={PersonalData}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="Staff"
+              component={Staff}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen
+              name="RecordTemps"
+              component={RecordTemps}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </isLoadingContext.Provider>
     </UserContext.Provider>
   );
 }
