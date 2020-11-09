7be07de5653c34e872f6d063a73e015d4727bc4a
Blaine <blaine.prestenbach@selu.edu>
2020-11-09 14:05:55 -0600

    Added ApiGetTempsBySchoolId

---
+++ FA20-P05-Mobile/ApiCalls/ApiGetTempsBySchoolId.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { configuration } from "../BaseUrl";
+
+function ApiGetTempsBySchoolId(schoolId, date) {
+  const url = `${configuration.BASE_URL}/api/schools/${schoolId}`;
+  let dateTime = date.toJSON(); // JavaScript date as a c# DateTime
+
+  return axios.get(url, { schoolId, dateTime });
+}
+
+export default ApiGetTempsBySchoolId;
