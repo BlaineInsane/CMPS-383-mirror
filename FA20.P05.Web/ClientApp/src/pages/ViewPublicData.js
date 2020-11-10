ce8e9e9a541833b4aedf356de6ebbd17f2b64090
Joseph Fontenelle <joseph.fontenelle@selu.edu>
2020-11-10 16:25:24 -0600

    styling
    need to fix view all button

--- FA20.P05.Web/ClientApp/src/pages/ViewPublicData.js
+++ FA20.P05.Web/ClientApp/src/pages/ViewPublicData.js
@@ -2,20 +2,31 @@ import React from "react";
 import { Form, Button } from "react-bootstrap";
 const ViewPublicData = () => (
   <div>
-    <h1 style={{ color: 'white' }}>View Public Data</h1>
-    <Form class= "PublicData">
-      <label style={{ color: 'white' }}>
+    <h1 style={{ color: "white" }}>View Public Data</h1>
+    <Form class="PublicData">
+      <label style={{ color: "white" }}>
         Day(mm/dd/year) : <br></br> &nbsp;<input type="text"></input>
       </label>
       <br></br>
-      <label style= {{ color: 'white' }}>
+      <label style={{ color: "white" }}>
         School : <br></br> &nbsp;<input type="text"></input>
       </label>
-      <br></br>  
+      <br></br>
       <Button block bsSize="large" type="submit">
         Search
       </Button>
     </Form>
+    <div
+      align="center"
+      style={{
+        backgroundColor: "blue",
+        width: "23%",
+      }}
+    >
+      <Button block bsSize="large" type="submit">
+        View All
+      </Button>
+    </div>
   </div>
 );
 export default ViewPublicData;
