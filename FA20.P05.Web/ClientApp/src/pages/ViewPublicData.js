e73ba02030763236ff02c2583ba4bb1133d37a21
heidiWesty <heidi.diggs@selu.edu>
2020-11-15 18:07:09 -0600

    getting 200 for get for view public data- working on displaying

--- FA20.P05.Web/ClientApp/src/pages/ViewPublicData.js
+++ FA20.P05.Web/ClientApp/src/pages/ViewPublicData.js
@@ -1,10 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import moment from "moment";
 
+import ApiGetTempsBySchoolId from "../ApiCalls/ApiGetTempsBySchoolId";
 import { activeSchoolsContext } from "../Context/ActiveSchoolsContext";
 
-function ViewPublicData() {
+export default function ViewPublicData() {
   const { activeSchools } = useContext(activeSchoolsContext);
+  const [datePicked, setDatePicked] = useState("");
+  const [schoolId, setId] = useState();
+  const [healthyTemps, setHealthyTemps] = useState("");
+  const [unhealthyTemps, setUnhealthyTemps] = useState("");
+  const [schoolPickedValue, setSchoolPickedValue] = useState();
 
   const SchoolDropBoxList = activeSchools.map((school) => {
     return (
@@ -14,6 +21,36 @@ function ViewPublicData() {
     );
   });
 
+  const idBySchool = activeSchools.map((school) => {
+    return school.id;
+  });
+
+  const handleSchoolId = () => {
+    setId(
+      schoolPickedValue != null ? schoolPickedValue : parseFloat(idBySchool)
+    );
+  };
+
+  const handleDateChange = (date) => {
+    if (moment(date).isAfter(moment(Date.now()), "day")) {
+      alert("Error", "Please select a non-future date.");
+    } else {
+      setDatePicked(date);
+    }
+  };
+
+  const handleConfirm = async () => {
+    try {
+      let res = await ApiGetTempsBySchoolId(schoolId, datePicked);
+      if (res.status === 200) {
+        setHealthyTemps(res.data.numHealthyTemps);
+        setUnhealthyTemps(res.data.numUnhealthyTemps);
+        console.log(res.status);
+        alert("hiya buddy");
+      }
+    } catch {}
+  };
+
   return (
     <div className="bg">
       <div className="out">
@@ -22,20 +59,25 @@ function ViewPublicData() {
             <div className="backpublic">
               <h1>View Public Data</h1>
             </div>
-            <Form>
+            <Form onSubmit={handleConfirm}>
               <br></br>
               <label style={{ color: "white" }} htmlFor="start">
                 {" "}
                 Date:
               </label>{" "}
               <br></br>
-              <input type="date" id="start" name="trip-start" />
+              <input
+                type="date"
+                id="start"
+                name="trip-start"
+                onChange={handleDateChange}
+              />
               <br></br>
               <label style={{ color: "white" }} htmlFor="school">
                 Choose a School:
               </label>
               <br></br>
-              <select name="school" id="cars">
+              <select name="school" id="cars" onChange={handleSchoolId}>
                 {SchoolDropBoxList}
               </select>
               <br></br>
@@ -56,4 +98,3 @@ function ViewPublicData() {
     </div>
   );
 }
-export default ViewPublicData;
